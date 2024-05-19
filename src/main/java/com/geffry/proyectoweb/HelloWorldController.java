6b7e355881d18bd13ade37aead519d7a57458d58
Geffry Alejandro Ospina Atehortúa <geffry.ospina@gmail.com>
2024-05-18 21:07:25 -0500

    add: Se agrego thymeleaf para mostrar template html

---
+++ src/main/java/com/geffry/proyectoweb/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.geffry.proyectoweb;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+    @GetMapping
+    public String helloWorld(Model model){
+        return "helloworld";
+    }
+}
